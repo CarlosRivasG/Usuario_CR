5ad0f80738414927df6348b0e80cccf805446357
CarlosR <cmrivas20@gmail.com>
2022-09-12 15:22:26 -0300

    CRUD

--- server.py
+++ server.py
@@ -11,9 +11,6 @@ def users():
     print("user: ", usuario)
     return render_template("index.html", list_users = usuario)
 
-
-    
-
 @app.route('/user/new')
 def new():
     return render_template("adduser.html")
@@ -24,7 +21,31 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+@app.route("/users/detalle/<id>")
+def users_detalle(id):
 
+    return render_template("detalles.html", user=User.get_by_id(id))
+
+@app.route('/user/updated/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("updated_user.html",user=User.get_one(data))
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+
+@app.route('/user/eliminar/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 
 if __name__=="__main__":
